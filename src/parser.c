bb11c7ed7fda1bd7fd8f52b82c73fefb08455085
MassinissaGhoul <massigh680@gmail.com>
2024-12-22 03:30:31 +0100

    lecture depuis ficher

--- src/parser.c
+++ src/parser.c
@@ -23,6 +23,13 @@ typedef struct Graph {
 
 } Graph;
 
+void freeNode(Node *node);
+void freeGraph(Graph *graph);
+void addArc(Graph *graph, int vertex1, int vertex2);
+Graph *readGraphFromFile(const char *filename, int directed);
+
+Graph *createGraph(int numNodes, int directed);
+
 void freeNode(Node *node) {
     if (node->otherNode) {
         free(node->otherNode);
@@ -50,6 +57,7 @@ void freeGraph(Graph *graph) {
 void addArc(Graph *graph, int vertex1, int vertex2) {
 
     // TODO
+    printf("Arc to add: %d -> %d\n", vertex1, vertex2);
 }
 Graph *readGraphFromFile(const char *filename, int directed) {
     int numNodes, numEdges;
@@ -98,19 +106,36 @@ Graph *createGraph(int numNodes, int directed) {
     graph->arcs = NULL;
     return graph;
 }
-
 int main() {
-
+    // Test 1 : Création manuelle du graphe
+    printf("\n=== Test 1: Création manuelle du graphe ===\n");
     Graph *graph = createGraph(5, 0);
     printf("graph with %d nodes \n", graph->numNodes);
     for (int i = 0; i < graph->numNodes; i++) {
         printf("Node %d, neighbor %d, color %d \n", graph->nodes[i].id,
                graph->nodes[i].nbNeighbor, graph->nodes[i].color);
     }
-
     freeGraph(graph);
     if (graph) {
-        printf("Graph memory free");
+        printf("Graph memory freed\n");
     }
+
+    // Test 2 : Lecture depuis un fichier
+    printf("\n=== Test 2: Lecture depuis un fichier ===\n");
+    Graph *graphFromFile = readGraphFromFile("graph/graphTest.col", 0);
+    if (graphFromFile) {
+        printf("Graph from file with %d nodes\n", graphFromFile->numNodes);
+        for (int i = 0; i < graphFromFile->numNodes; i++) {
+            printf("Node %d, neighbor %d, color %d \n",
+                   graphFromFile->nodes[i].id,
+                   graphFromFile->nodes[i].nbNeighbor,
+                   graphFromFile->nodes[i].color);
+        }
+        freeGraph(graphFromFile);
+        printf("Graph from file memory freed\n");
+    } else {
+        printf("Failed to read graph from file\n");
+    }
+
     return 0;
 }
