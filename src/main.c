167e12ab469d580477de38dcfda2141d73ec72f7
TenixOue <79940202+TenixOue@users.noreply.github.com>
2025-01-07 10:53:06 +0100

    interface utilisateur

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include "main.h"
 #include "welshPowell.h"
 #include "hillClimbing.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 double runAlgorithm(int (*algorithm)(Graph *), Graph *graph, int times)
 {
@@ -28,61 +31,131 @@ double runAlgorithm(int (*algorithm)(Graph *), Graph *graph, int times)
     }
     return average;
 }
-
-int main(int argc, char *argv[])
-{
-    /*
-    // Test 1 : Création manuelle du graphe
-    printf("\n=== Test 1: Creation manuelle du graphe ===\n");
-    Graph *graph = createGraph(5, 0);
-    // graphToString(graph);
-    freeGraph(graph);
-    if (graph) {
-        printf("Graph memory freed\n");
+// Menu principal
+int mainMenu() {
+    int choice;
+    printf("\n=== Menu Principal ===\n");
+    printf("1. Colorer un graphe\n");
+    printf("2. Verifier une coloration\n");
+    printf("Votre choix: ");
+    scanf("%d", &choice);
+    if (choice != 1 && choice != 2) {
+        fprintf(stderr, "Choix invalide. Veuillez selectionner 1 ou 2.\n");
+        exit(EXIT_FAILURE);
     }
-    */
+    return choice;
+}
 
-    if (argc != 4 && argc != 5)
-    {
-        fprintf(stderr, "Nombre de parametres invalide. Utilisation : \"./color_graph.exe <dossier/fichier.extension> <dsatur | welshpowell> <nb_executions> opt : <sauvegarder>\n");
-        exit(-1);
-    }
+// Selection de l'algorithme
+int selectAlgorithm() {
+    int choice;
+    printf("\nChoisissez l'algorithme de coloration:\n");
+    printf("1. DSATUR\n");
+    printf("2. Welsh-Powell\n");
+    printf("Votre choix: ");
+    scanf("%d", &choice);
 
-    int (*algo)(Graph *);
-    if (!strcmp(argv[2], "dsatur"))
-    {
-        algo = &colorGraphDSatur;
+    if (choice != 1 && choice != 2) {
+        fprintf(stderr, "Choix invalide.\n");
+        exit(EXIT_FAILURE);
     }
-    else if (!strcmp(argv[2], "welshpowell"))
-    {
-        algo = &welshPowell;
-    }
-    else
-    {
-        fprintf(stderr, "Algorithme inconnu ou mal tape. Veuillez choisir parmis ceux connus <dsatur | welshpowell>\n");
-        exit(-1);
-    }
+    return choice;
+}
 
-    // Test 3 : Lecture depuis un fichier
-    printf("\n=== Coloration de %s par l'algorithme %s ===\n", argv[1], argv[2]);
-    Graph *graphFromFile = readGraphFromFile(argv[1], 0);
-    if (graphFromFile)
-    {
-        graphToString(graphFromFile, 0);
-        printf("Temps d'execution moyen : %lf ms\n", runAlgorithm(algo, graphFromFile, atoi(argv[3])));
-        printf("Est-ce que la coloration est valide : %d\n", checkColoring(graphFromFile));
-        printf("Nombre de couleurs utilisees : %d\n", biggestColor(graphFromFile));
+// Sauvegarde des resultats
+void saveResult(Graph *graph) {
+    int saveChoice;
+    printf("\nVoulez-vous sauvegarder la coloration? (1: Oui, 0: Non): ");
+    scanf("%d", &saveChoice);
+    if (saveChoice) {
+        char format[5];
+        printf("Choisissez le format (json/col): ");
+        scanf("%s", format);
+        char filename[256];
+        printf("Entrez le nom du fichier de sauvegarde (sans extension): ");
+        scanf("%s", filename);
 
-        if (argc == 5)
-        {
-            if (!strcmp(argv[4], "sauvegarder"))
-            {
-                saveColoredGraph("../representation/graphes/resultat.json", graphFromFile);
-            }
+        // Ajout du chemin "save/" devant le nom du fichier
+        char fullPath[300] = "save/";
+        strcat(fullPath, filename);
+
+        if (strcmp(format, "json") == 0) {
+            strcat(fullPath, ".json");
+            saveColoredGraph(fullPath, graph);
+        } else if (strcmp(format, "col") == 0) {
+            strcat(fullPath, ".col");
+            saveGraph(fullPath, graph);
+        } else {
+            fprintf(stderr, "Format de fichier invalide.\n");
         }
 
-        freeGraph(graphFromFile);
+        printf("Fichier sauvegarde dans : %s\n", fullPath);
+    }
+}
+
+
+// Verification de la validite de la coloration
+void checkColoringInput() {
+    char fileName[256];
+    printf("\nEntrez le nom du fichier .col a verifier (ex: bigGraph.col): ");
+    scanf("%s", fileName);
+
+    char filePath[300] = "graph/";
+    strcat(filePath, fileName);
+
+    Graph *graph = readGraphFromFile(filePath, 0);
+    if (!graph) {
+        fprintf(stderr, "Erreur: Impossible de lire le fichier.\n");
+        exit(EXIT_FAILURE);
     }
 
+    int valid = checkColoring(graph);
+    printf("La coloration est %s.\n", valid ? "valide" : "invalide");
+    freeGraph(graph);
+}
+
+
+// Fonction pour colorer un graphe
+void colorGraph() {
+    char fileName[256];
+    printf("\nEntrez le nom du fichier .col a utiliser (ex: myGraph.col): ");
+    scanf("%s", fileName);
+
+    char fullPath[256] = "graph/";
+    strcat(fullPath, fileName);
+
+    Graph *graph = readGraphFromFile(fullPath, 0);
+    if (!graph) {
+        fprintf(stderr, "Erreur: Impossible de charger le graphe.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int choice = selectAlgorithm();
+    int (*algorithm)(Graph *) = (choice == 1) ? colorGraphDSatur : welshPowell;
+
+    int nbExecutions;
+    printf("Entrez le nombre d'executions pour mesurer la performance: ");
+    scanf("%d", &nbExecutions);
+
+    printf("\nExecution de l'algorithme...\n");
+    double execTime = runAlgorithm(algorithm, graph, nbExecutions);
+    printf("Temps d'execution moyen: %.2f ms\n", execTime);
+    printf("Nombre de couleurs utilisees: %d\n", biggestColor(graph));
+    printf("Coloration valide: %s\n", checkColoring(graph) ? "Oui" : "Non");
+
+    saveResult(graph);
+    freeGraph(graph);
+}
+
+int main() {
+    int choice = mainMenu();
+
+    if (choice == 1) {
+        colorGraph();  // Coloration d'un graphe
+    } else if (choice == 2) {
+        checkColoringInput();  // Verification d'une coloration
+    }
+
+    printf("\nFin du programme.\n");
     return 0;
 }
